12a208b153d3a0c2295712d6f3edb534f8b21c73
Aksun <Yusuf.Aksun@thomsonreuters.com>
2020-10-24 18:53:29 +0100

    vpc as module

--- terraform-code/01-vpc.tf
+++ terraform-code/01-vpc.tf
@@ -1,10 +1,28 @@
 module "vpc" {
-  source     = "git::https://github.com/tmknom/terraform-aws-vpc.git?ref=tags/2.0.1"
-  cidr_block = "10.255.0.0/16"
-  name       = "example-vpc"
+  source = "terraform-aws-modules/vpc/aws"
 
-  public_subnet_cidr_blocks  = ["10.255.0.0/24", "10.255.1.0/24"]
-  public_availability_zones  = ["us-east-1a", "us-east-1b"]
-  private_subnet_cidr_blocks = ["10.255.64.0/24", "10.255.65.0/24"]
-  private_availability_zones = ["us-east-1a", "us-east-1b"]
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway  = true
+  single_nat_gateway  = false
+  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
+  external_nat_ip_ids = "${aws_eip.nat.*.id}"   # <= IPs specified here as input to the module
+  
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_eip" "nat" {
+  count = 3
+
+  vpc = true
 }
